ec55d6275ec1f0cb2af1756fc9f479595be88df0
Cornelia Bodea <cornelia.bodea.work@gmail.com>
2023-03-23 18:11:02 +0200

    Fix cors problems on local

--- src/app.ts
+++ src/app.ts
@@ -26,12 +26,7 @@ AppDataSource.initialize()
 
 		if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-		app.use(
-			cors({
-				origin: config.get<string>('origin'),
-				credentials: false,
-			})
-		);
+		app.use(cors());
 
 		app.use('/api/users', userRouter);
 		app.use('/api/roles', roleRouter);
